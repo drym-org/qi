639693b40a02718c42dcdabfbaafb5a060de5c88
Siddhartha <sid@countvajhula.com>
2021-04-01 21:16:08 -0700

    tests for the remaining cases handled

--- syntax/on.rkt
+++ syntax/on.rkt
@@ -97,7 +97,114 @@
      (check-equal? (on (5)
                        [positive? 1 2 3])
                    3
-                   "more than one body form"))))
+                   "more than one body form")
+     (check-equal? (on (5)
+                       [(eq? 5) 'five]
+                       [else 'not-five])
+                   'five
+                   "eq?")
+     (check-equal? (on (6)
+                       [(eq? 5) 'five]
+                       [else 'not-five])
+                   'not-five
+                   "eq?")
+     (check-equal? (on ("hello")
+                       [(equal? "hello") 'hello]
+                       [else 'not-hello])
+                   'hello
+                   "equal?")
+     (check-equal? (on ("bye")
+                       [(equal? "hello") 'hello]
+                       [else 'not-hello])
+                   'not-hello
+                   "equal?")
+     (check-equal? (on (5)
+                       [(and positive? integer?) 'yes]
+                       [else 'no])
+                   'yes
+                   "and (conjoin)")
+     (check-equal? (on (5.4)
+                       [(and positive? integer?) 'yes]
+                       [else 'no])
+                   'no
+                   "and (conjoin)")
+     (check-equal? (on (5.3)
+                       [(or positive? integer?) 'yes]
+                       [else 'no])
+                   'yes
+                   "or (disjoin)")
+     (check-equal? (on (-5.4)
+                       [(or positive? integer?) 'yes]
+                       [else 'no])
+                   'no
+                   "or (disjoin)")
+     (check-equal? (on (-5)
+                       [(not positive?) 'yes]
+                       [else 'no])
+                   'yes
+                   "not (predicate negation)")
+     (check-equal? (on (5)
+                       [(not positive?) 'yes]
+                       [else 'no])
+                   'no
+                   "not (predicate negation)")
+     (check-equal? (on (5 "hi")
+                       [(and-jux positive? string?) 'yes]
+                       [else 'no])
+                   'yes
+                   "juxtaposed conjoin")
+     (check-equal? (on (5 5)
+                       [(and-jux positive? string?) 'yes]
+                       [else 'no])
+                   'no
+                   "juxtaposed conjoin")
+     (check-equal? (on (5 "hi")
+                       [(or-jux positive? string?) 'yes]
+                       [else 'no])
+                   'yes
+                   "juxtaposed disjoin")
+     (check-equal? (on (-5 "hi")
+                       [(or-jux positive? string?) 'yes]
+                       [else 'no])
+                   'yes
+                   "juxtaposed disjoin")
+     (check-equal? (on (-5 5)
+                       [(or-jux positive? string?) 'yes]
+                       [else 'no])
+                   'no
+                   "juxtaposed disjoin")
+     (check-equal? (on (5)
+                       [positive? (call add1)]
+                       [else 'no])
+                   6
+                   "on-call")
+     (check-equal? (on (-5)
+                       [positive? (call add1)]
+                       [else 'no])
+                   'no
+                   "on-call")
+     (check-equal? (on (3 5)
+                       [< (call +)]
+                       [else 'no])
+                   8
+                   "on-call n-ary predicate")
+     (check-equal? (on (3 5)
+                       [> (call +)]
+                       [else 'no])
+                   'no
+                   "on-call n-ary predicate")
+     (check-equal? (on (-3 5)
+                       [> (call +)]
+                       [(or-jux positive? integer?) 'yes]
+                       [else 'no])
+                   'yes
+                   "heterogeneous clauses")
+     (check-equal? (on (-3 5)
+                       [> (call +)]
+                       [(and-jux positive? integer?) 'yes]
+                       [else 'no])
+                   'no
+                   "heterogeneous clauses"))))
 
 (module+ test
   (run-tests tests))
