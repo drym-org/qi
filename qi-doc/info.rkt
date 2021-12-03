8cc0c7143602c80217a4036541094f610008bce6
Siddhartha <sid@countvajhula.com>
2021-12-03 13:05:52 -0800

    add missing doc dependencies

--- qi-doc/info.rkt
+++ qi-doc/info.rkt
@@ -4,15 +4,17 @@
 (define deps '("base"))
 (define build-deps '("scribble-lib"
                      "scribble-abbrevs"
+                     "scribble-math"
                      "racket-doc"
                      "sandbox-lib"
-                     "fancy-app"
-                     "math-lib"
+                     "from-template"
                      "quickscript"
                      "quickscript-extra"
                      "qi-lib"
                      "Qi-Quickscripts"
                      "threading-doc"
+                     "fancy-app"
+                     "math-lib"
                      "relation"))
 (define scribblings '(("scribblings/qi.scrbl" (multi-page))))
 (define clean '("compiled" "doc" "doc/qi"))
