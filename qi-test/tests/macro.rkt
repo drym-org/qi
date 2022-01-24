35f6c1d753060711c296d4b06ac1b4ba4b20461c
Siddhartha <sid@countvajhula.com>
2022-01-24 12:32:40 -0800

    add `pare` macro, to serve as a canonical macro example

--- qi-test/tests/macro.rkt
+++ qi-test/tests/macro.rkt
@@ -11,6 +11,9 @@
 (define-qi-syntax-rule (square flo:expr)
   (feedback 2 flo))
 
+(define-qi-syntax-rule (pare car-flo cdr-flo)
+  (group 1 car-flo cdr-flo))
+
 (define-qi-syntax-parser cube
   [(_ flo) #'(feedback 3 flo)])
 
@@ -24,6 +27,7 @@
   (test-suite
    "macro tests"
    (check-equal? ((☯ (square sqr)) 2) 16)
+   (check-equal? ((☯ (~> (pare sqr +) ▽)) 3 6 9) (list 9 15))
    (check-equal? ((☯ (cube sqr)) 2) 256)
    (check-equal? ((☯ (fanout 5)) 2) 'hello "extensions can override built-in forms")
    (check-equal? ((☯ kazam) 2) 'hello "extensions can add identifier macros")))
