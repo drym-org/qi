6d6698a6650b7ec55c88bf7d334a861bb743be02
github-action-benchmark <github@users.noreply.github.com>
2022-06-24 07:37:48 +0000

    add Qi Forms Benchmarks (customSmallerIsBetter) benchmark result for bdbd8d09aa8deb5ba41d0c9d9f049b02e8ddc567

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1656056268795,
+  "repoUrl": "https://github.com/countvajhula/qi",
+  "entries": {
+    "Qi Forms Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "sid@countvajhula.com",
+            "name": "Siddhartha",
+            "username": "countvajhula"
+          },
+          "committer": {
+            "email": "sid@countvajhula.com",
+            "name": "Siddhartha",
+            "username": "countvajhula"
+          },
+          "distinct": true,
+          "id": "bdbd8d09aa8deb5ba41d0c9d9f049b02e8ddc567",
+          "message": "add `cli` dependency",
+          "timestamp": "2022-06-24T00:31:01-07:00",
+          "tree_id": "46bd67d9656275f840eafc711adcd7593801b706",
+          "url": "https://github.com/countvajhula/qi/commit/bdbd8d09aa8deb5ba41d0c9d9f049b02e8ddc567"
+        },
+        "date": 1656056267888,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pass",
+            "value": 187,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
