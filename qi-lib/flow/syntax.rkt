0b0c07a7f166204e2d7d0e5a0508b80e86e1dcb6
Siddhartha <sid@countvajhula.com>
2022-07-08 12:09:09 -0700

    working state of refactored phases (pair session with MB)

--- qi-lib/flow/syntax.rkt
+++ qi-lib/flow/syntax.rkt
@@ -1,10 +1,6 @@
 #lang racket/base
 
-(provide literal
-         subject
-         clause
-         starts-with
-         sep-form
+(provide sep-form
          select-form
          block-form
          group-form
@@ -28,44 +24,8 @@
          right-threading-form
          clos-form)
 
-(require syntax/parse
-         racket/string)
-
-(define-syntax-class literal
-  (pattern
-   (~or* expr:boolean
-         expr:char
-         expr:string
-         expr:bytes
-         expr:number
-         expr:regexp
-         expr:byte-regexp
-         ;; We'd like to treat quoted forms as literals as well. This
-         ;; includes symbols, and would also include, for instance,
-         ;; syntactic specifications of flows, since flows are
-         ;; syntactically lists as they inherit the elementary syntax of
-         ;; the underlying language (Racket). Quoted forms are read as
-         ;; (quote ...), so we match against this
-         ((~datum quote) expr:expr)
-         ((~datum quasiquote) expr:expr)
-         ((~datum quote-syntax) expr:expr)
-         ((~datum syntax) expr:expr))))
-
-(define-syntax-class subject
-  #:attributes (args arity)
-  (pattern
-   (arg:expr ...)
-   #:with args #'(arg ...)
-   #:attr arity (length (syntax->list #'args))))
-
-(define-syntax-class clause
-  (pattern
-   expr:expr))
-
-(define-syntax-class (starts-with pfx)
-  (pattern
-   i:id #:when (string-prefix? (symbol->string
-                                (syntax-e #'i)) pfx)))
+(require "aux-syntax.rkt"
+         syntax/parse)
 
 #|
 These syntax classes are used in the flow macro to handle matching of
@@ -179,7 +139,7 @@ See comments in flow.rkt for more details.
    ((~datum loop) arg ...)))
 
 (define-syntax-class blanket-template-form
-  ;; "prarg" = "pre-supplied argument"
+  ;; " prarg" = "pre-supplied argument"
   (pattern
    (natex prarg-pre ... (~datum __) prarg-post ...)))
 
