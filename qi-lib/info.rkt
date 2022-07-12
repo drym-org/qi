2ebb86502dc0e1165bbc58a7b11ba474c7ea7f71
D. Ben Knoble <ben.knoble+github@gmail.com>
2022-07-12 10:24:20 -0400

    remove typed-stack dependency
    It was used for one purpose only: a stack abstraction in `except-args`.
    Fortunately, the operations `top`, `stack-empty?` and `pop` (without
    mutation) are represented by the purely functional `first`, `empty?`,
    and `rest` over (linked) lists; we simulate mutation by carrying the
    stack in the `let loop`.
    
    This was found to be one of the heaviest remaining dependencies. At this
    point, qi-lib only depends on "base" and "fancy-app," both of which are
    necessary for the package to function as expected. It seems unlikely we
    will get any more startup performance gains by trimming dependencies.

--- qi-lib/info.rkt
+++ qi-lib/info.rkt
@@ -3,8 +3,7 @@
 (define version "2.0")
 (define collection "qi")
 (define deps '("base"
-               "fancy-app"
-               "typed-stack"))
+               "fancy-app"))
 (define build-deps '())
 (define clean '("compiled" "private/compiled"))
 (define pkg-authors '(countvajhula))
