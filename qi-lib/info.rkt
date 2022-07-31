585df5c09f2bc1505cd98489be572fe493937932
Siddhartha <sid@countvajhula.com>
2022-07-31 13:50:59 -0700

    depend on fancy-app 1.1

--- qi-lib/info.rkt
+++ qi-lib/info.rkt
@@ -3,7 +3,7 @@
 (define version "3.0")
 (define collection "qi")
 (define deps '("base"
-               "fancy-app"))
+               ("fancy-app" #:version "1.1")))
 (define build-deps '())
 (define clean '("compiled" "private/compiled"))
 (define pkg-authors '(countvajhula))
