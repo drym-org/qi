c46ce27e3e8af3f2ac7cffdc71842b82ffafa214
Siddhartha <sid@countvajhula.com>
2022-08-25 17:22:01 -0700

    extract more non-core forms

--- qi-lib/flow/extended/impl.rkt
+++ qi-lib/flow/extended/impl.rkt
@@ -1,5 +1,18 @@
 #lang racket/base
 
-(provide ->boolean)
+(require (only-in racket/function
+                  const))
+
+(provide ->boolean
+         true.
+         false.)
 
 (define (->boolean v) (and v #t))
+
+(define true.
+  (procedure-rename (const #t)
+                    'true.))
+
+(define false.
+  (procedure-rename (const #f)
+                    'false.))
