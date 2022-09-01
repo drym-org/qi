f3e80390aa4f65ccfc2cc68fbbac71e1e7f01caf
Siddhartha <sid@countvajhula.com>
2022-08-31 19:51:31 -0700

    Report source location info in `report-syntax-error`

--- qi-lib/private/util.rkt
+++ qi-lib/private/util.rkt
@@ -6,21 +6,25 @@
 
 (require racket/string
          racket/format
+         racket/match
          syntax/parse/define
          (for-syntax racket/base
                      syntax/parse/lib/function-header))
 
-(define (report-syntax-error name args usage . msgs)
-  (raise-syntax-error name
-                      (~a "Syntax error in "
-                          (list* name args)
-                          "\n"
-                          "Usage:\n"
-                          "  " usage
-                          (if (null? msgs)
-                              ""
-                              (string-append "\n"
-                                             (string-join msgs "\n"))))))
+(define (report-syntax-error stx usage . msgs)
+  (match (syntax->datum stx)
+    [(cons name args)
+     (raise-syntax-error name
+                         (~a "Syntax error in "
+                             (list* name args)
+                             "\n"
+                             "Usage:\n"
+                             "  " usage
+                             (if (null? msgs)
+                                 ""
+                                 (string-append "\n"
+                                                (string-join msgs "\n"))))
+                         stx)]))
 
 (define-syntax-parse-rule (define-alias alias:id name:id)
   (define-syntax alias (make-rename-transformer #'name)))
