1604ec94c58ca7911c2b9be8b5d3e139b6c5f4fc
Siddhartha <sid@countvajhula.com>
2022-09-10 08:51:16 -0700

    declare bindingspec dependency in a git url

--- qi-lib/info.rkt
+++ qi-lib/info.rkt
@@ -3,7 +3,10 @@
 (define version "3.0")
 (define collection "qi")
 (define deps '("base"
-               ("fancy-app" #:version "1.1")))
+               ("fancy-app" #:version "1.1")
+               ;; this git URL should be changed to a named package spec
+               ;; once bindingspec is on the package index
+               "git://github.com/michaelballantyne/bindingspec.git"))
 (define build-deps '())
 (define clean '("compiled" "private/compiled"))
 (define pkg-authors '(countvajhula))
