d99269088c99d999dd3759317e210ba474c77f3e
Siddhartha <sid@countvajhula.com>
2022-09-10 09:05:40 -0700

    fix git url

--- qi-lib/info.rkt
+++ qi-lib/info.rkt
@@ -6,7 +6,7 @@
                ("fancy-app" #:version "1.1")
                ;; this git URL should be changed to a named package spec
                ;; once bindingspec is on the package index
-               "git://github.com/michaelballantyne/bindingspec.git"))
+               "git://github.com/michaelballantyne/bindingspec.git#main"))
 (define build-deps '())
 (define clean '("compiled" "private/compiled"))
 (define pkg-authors '(countvajhula))
