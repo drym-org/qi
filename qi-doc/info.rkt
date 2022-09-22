99b228a0d8304debe0bc3183b6feda5d422ebb07
Siddhartha <sid@countvajhula.com>
2022-09-22 12:44:14 -0700

    add relation-lib to doc dependencies

--- qi-doc/info.rkt
+++ qi-doc/info.rkt
@@ -9,6 +9,7 @@
                      "racket-doc"
                      "sandbox-lib"
                      "metapict"
+                     "relation-lib"
                      "qi-lib"
                      "qi-probe"))
 (define scribblings '(("scribblings/qi.scrbl" (multi-page) (language))))
