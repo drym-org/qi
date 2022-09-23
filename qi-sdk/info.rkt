2f4b02cb2c8153b02e80fea74480bcda92e4c90e
Siddhartha <sid@countvajhula.com>
2022-09-23 09:35:13 -0700

    move some deps to make raco happy

--- qi-sdk/info.rkt
+++ qi-sdk/info.rkt
@@ -2,11 +2,15 @@
 
 (define version "3.0")
 (define collection "qi")
-(define deps '("base"))
-(define build-deps '("cli"
-                     "collections-lib"
-                     "relation-lib"
-                     "cover"
-                     "cover-coveralls"))
+(define deps '("base"
+               "qi-lib"
+               "adjutor"
+               "cli"
+               "math-lib"
+               "collections-lib"
+               "relation-lib"
+               "cover"
+               "cover-coveralls"))
+(define build-deps '())
 (define clean '("compiled" "private/compiled"))
 (define pkg-authors '(countvajhula))
