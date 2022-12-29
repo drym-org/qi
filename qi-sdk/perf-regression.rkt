1a6f3a28da01971611d681765335973e4d9982e6
Siddhartha <sid@countvajhula.com>
2022-12-29 01:36:12 -0800

    script to measure form performance regressions

---
+++ qi-sdk/perf-regression.rkt
@@ -0,0 +1,55 @@
+#!/usr/bin/env racket
+#lang cli
+
+(require qi
+         qi/probe)
+
+(require relation
+         json
+         racket/format
+         racket/port)
+
+(define (parse-json-file filename)
+  (call-with-input-file filename
+    (λ (port)
+      (read-json port))))
+
+(help
+ (usage (~a "Reports relative performance of forms between two sets of results\n"
+            "(e.g. run against two different commits).")))
+
+(program (main [before-file "'before' file"] [after-file "'after' file"])
+  (define before
+    (make-hash
+     (map (☯ (~> (-< (hash-ref 'name)
+                     (hash-ref 'value)) cons))
+          (parse-json-file before-file))))
+  (define after
+    (make-hash
+     (map (☯ (~> (-< (~> (hash-ref 'name)
+                         (switch
+                           [(equal? "foldr") "<<"]
+                           [(equal? "foldl") ">>"]
+                           [else _]))
+                     (hash-ref 'value)) cons))
+          (parse-json-file after-file))))
+  (define results
+    (~>> (before)
+         hash-keys
+         △
+         (><
+          (~>
+           (-< _
+               (~> (-< (hash-ref after _)
+                       (hash-ref before _))
+                   /
+                   (if (< 0.75 _ 1.5)
+                       1
+                       (~r #:precision 2))))
+           ▽))
+         ▽
+         (sort > #:key (☯ (~> cadr ->inexact)))))
+  ;; (write-json results)
+  (println results))
+
+(run main)
