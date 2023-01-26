02b885360647ecb8a355d9f36a9d75beadb5eb22
Siddhartha <sid@countvajhula.com>
2023-01-26 10:26:02 -0800

    macro to create value definitions in the qi binding space (pairing..)

--- qi-lib/flow/extended/forms.rkt
+++ qi-lib/flow/extended/forms.rkt
@@ -12,11 +12,23 @@
 (require (for-syntax racket/base
                      syntax/parse
                      (only-in racket/list make-list)
+                     syntax/parse/lib/function-header
                      "syntax.rkt"
                      "../aux-syntax.rkt")
+         syntax/parse/define
          "expander.rkt"
          "../../macro.rkt"
          "impl.rkt")
+
+(define-syntax-parser define-for-qi
+  [(_ name:id expr:expr)
+   #:with spaced-name ((make-interned-syntax-introducer 'qi) #'name)
+   #'(define spaced-name expr)]
+  [(_ (name:id . args:formals)
+      expr:expr ...)
+   #'(define-for-qi name
+       (lambda args
+         expr ...))])
 
 ;;; Predicates
 
