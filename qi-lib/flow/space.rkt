315417565e1573dce2da569f9ac2ba5bb8409f0a
Siddhartha <sid@countvajhula.com>
2023-02-23 22:58:01 -0800

    add an explanatory comment re: bindings in qi space

--- qi-lib/flow/space.rkt
+++ qi-lib/flow/space.rkt
@@ -6,6 +6,15 @@
          (for-syntax racket/base
                      syntax/parse/lib/function-header))
 
+;; Define variables in the qi binding space.
+;; This allows us to define functions in the qi space which, when used in
+;; qi contexts, would not be shadowed by bindings at the use site. This
+;; gives us some of the benefits of core linguistic forms while also not
+;; actually inflating the size of the core language nor incurring the
+;; performance penalty it might if it were implemented as a macro
+;; compiling to the core language.
+;; See "A loophole in Qi space":
+;;   https://github.com/drym-org/qi/wiki/Qi-Compiler-Sync-Jan-26-2023
 (define-syntax-parser define-for-qi
   [(_ name:id expr:expr)
    #:with spaced-name ((make-interned-syntax-introducer 'qi) #'name)
