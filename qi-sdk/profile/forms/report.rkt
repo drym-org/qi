71e0024ca3346f52e6d333eea01ce044e9c3464b
Siddhartha <sid@countvajhula.com>
2023-03-07 20:05:40 -0800

    Organize scripts to generate performance reports
    Have separate scripts to generate reports of different aspects, to
    preserve separation of concerns. This results in some duplication of
    configuration, though, which is probably best avoided via some
    improvements to the cli library.
    
    Follow the general pattern of echoing live results to STDERR and
    actual formatted output to STDOUT.

--- qi-sdk/profile/forms/report.rkt
+++ qi-sdk/profile/forms/report.rkt
@@ -1,20 +1,19 @@
 #!/usr/bin/env racket
 #lang cli
 
-(require "loading/loadlib.rkt"
-         "regression.rkt")
+(require "../regression.rkt")
 
 (require racket/match
          racket/format
          relation
          qi
-         (only-in "util.rkt"
+         (only-in "../util.rkt"
                   only-if
                   for/call
                   write-csv
                   format-output))
 (require
- (submod "forms/benchmarks.rkt" main))
+ (submod "benchmarks.rkt" main))
 
 (flag (forms #:param [forms null] name)
   ("-f" "--form" "Forms to benchmark")
@@ -34,28 +33,12 @@
    "Output format to use, either 'json' or 'csv'. If none is specified, no output is generated.")
   (output-format fmt))
 
-(flag (type #:param [report-type "all"] typ)
-  ("-t"
-   "--type"
-   "Type of report, either `forms`, `loading` or `all` (default `all`)")
-  (report-type typ))
-
 (flag (regression-file #:param [regression-file #f] reg-file)
   ("-r" "--regression" "'Before' data to compute regression against")
   (regression-file reg-file))
 
-;; Note: much of this file is duplicated across forms/report.rkt
-;; and loading/report.rkt. It could be avoided if we had
-;; "composition of commands", see:
-;; https://github.com/countvajhula/cli/issues/3
 (program (main)
-  (let* ([forms-data (if (member? (report-type) (list "all" "forms"))
-                         (benchmark (forms))
-                         null)]
-         [require-data (if (member? (report-type) (list "all" "loading"))
-                           (list (profile-load "qi"))
-                           null)]
-         [output (append forms-data require-data)])
+  (let ([output (benchmark (forms))])
     (if (regression-file)
         (let ([before (parse-benchmarks (parse-json-file (regression-file)))]
               [after (parse-benchmarks output)])
