71c638e7e0b8cc7d689d3413b60aea093d6bf169
Siddhartha <sid@countvajhula.com>
2023-03-15 01:26:34 -0700

    use regression logic to implement competitive benchmarks

---
+++ qi-sdk/profile/nonlocal/report-competitive.rkt
@@ -0,0 +1,46 @@
+#!/usr/bin/env racket
+#lang cli
+
+(require racket/match
+         racket/format
+         relation
+         qi
+         (only-in "../util.rkt"
+                  only-if
+                  for/call
+                  write-csv
+                  format-output)
+         "../regression.rkt"
+         "intrinsic.rkt")
+
+(flag (selected #:param [selected null] name)
+  ("-s" "--select" "Select benchmark by name")
+  (selected (cons name (selected))))
+
+(constraint (multi selected))
+
+(help
+ (usage
+  (~a "Run competitive benchmarks between Qi and Racket, "
+      "reporting the results in a configurable output format.")))
+
+(flag (output-format #:param [output-format ""] fmt)
+  ("-f"
+   "--format"
+   "Output format to use, either 'json' or 'csv'. If none is specified, no output is generated.")
+  (output-format fmt))
+
+(program (main)
+  (displayln "\nRunning competitive benchmarks..." (current-error-port))
+
+  (let* ([racket-output (benchmark "racket" (selected))]
+         [qi-output (benchmark "qi" (selected))]
+         [before (parse-benchmarks racket-output)]
+         [after (parse-benchmarks qi-output)])
+    (format-output (compute-regression before after)
+                   (output-format))))
+
+;; To run benchmarks for a form interactively, use e.g.:
+;; (run main #("-s" "composition"))
+
+(run main)
