551b9a14fa2262c7a94cc77d4481632fda02a197
Siddhartha <sid@countvajhula.com>
2023-03-15 00:30:53 -0700

    continue reorganizing benchmarks..

--- qi-sdk/profile/local/report.rkt
+++ qi-sdk/profile/local/report.rkt
@@ -10,10 +10,8 @@
                   for/call
                   write-csv
                   format-output)
-
-         "loading/loadlib.rkt"
          "../regression.rkt"
-         (submod "forms/benchmarks.rkt" main))
+         (submod "benchmarks.rkt" main))
 
 (flag (selected #:param [selected null] name)
   ("-s" "--select" "Select form to benchmark")
@@ -33,29 +31,15 @@
    "Output format to use, either 'json' or 'csv'. If none is specified, no output is generated.")
   (output-format fmt))
 
-(flag (type #:param [report-type "all"] typ)
-  ("-t"
-   "--type"
-   "Type of report, either `forms`, `loading` or `all` (default `all`)")
-  (report-type typ))
-
 (flag (regression-file #:param [regression-file #f] reg-file)
   ("-r" "--regression" "'Before' data to compute regression against")
   (regression-file reg-file))
 
-;; Note: much of this file is duplicated across forms/report.rkt
-;; and loading/report.rkt. It could be avoided if we had
-;; "composition of commands", see:
-;; https://github.com/countvajhula/cli/issues/3
 (program (main)
-  (let* ([forms-data (if (member? (report-type) (list "all" "forms"))
-                         (benchmark (selected))
-                         null)]
-         [require-data (if (member? (report-type) (list "all" "loading"))
-                           (list (profile-load "qi"))
-                           null)]
-         [output (append forms-data require-data)])
+  (let ([output (benchmark (selected))])
     (if (regression-file)
+        ;; TODO: regression ignores any flags and is a parallel path
+        ;; it should be properly incorporated into the CLI
         (let ([before (parse-benchmarks (parse-json-file (regression-file)))]
               [after (parse-benchmarks output)])
           (compute-regression before after))
