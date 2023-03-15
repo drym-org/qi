4f5660337010d98ef5526c100066ed05098c9687
Siddhartha <sid@countvajhula.com>
2023-03-14 23:54:47 -0700

    support selecting specific nonlocal benchmarks to run via CLI

--- qi-sdk/profile/competitive/intrinsic.rkt
+++ qi-sdk/profile/competitive/intrinsic.rkt
@@ -6,14 +6,18 @@
 (require "../util.rkt"
          "spec.rkt")
 
-(define (benchmark language)
-  (define namespace (make-base-namespace))
-  (cond [(eq? 'qi language) (eval '(require "qi/main.rkt") namespace)]
-        [(eq? 'racket language) (eval '(require "racket/main.rkt") namespace)])
+(define (benchmark language benchmarks-to-run)
+  (let ([namespace (make-base-namespace)]
+        [benchmarks-to-run (if (null? benchmarks-to-run)
+                               (map bm-name specs)
+                               benchmarks-to-run)])
+    (cond [(eq? 'qi language) (eval '(require "qi/main.rkt") namespace)]
+          [(eq? 'racket language) (eval '(require "racket/main.rkt") namespace)])
 
-  (for/list ([spec specs])
-    (let ([name (bm-name spec)]
-          [exerciser (bm-exerciser spec)]
-          [f (eval (read (open-input-string (bm-target spec))) namespace)]
-          [n-times (bm-times spec)])
-      (run-nonlocal-benchmark name exerciser f n-times))))
+    (for/list ([spec specs]
+               #:when (member (bm-name spec) benchmarks-to-run))
+      (let ([name (bm-name spec)]
+            [exerciser (bm-exerciser spec)]
+            [f (eval (read (open-input-string (bm-name spec))) namespace)]
+            [n-times (bm-times spec)])
+        (run-nonlocal-benchmark name exerciser f n-times)))))
