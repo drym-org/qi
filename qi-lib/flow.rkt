91f2b46cd0a7f2584ad05ee3093ab4fc319ac36d
Siddhartha <sid@countvajhula.com>
2023-09-01 17:33:34 -0700

    use the much-anticipated syntax-spec-v1 from the package index :)

--- qi-lib/flow.rkt
+++ qi-lib/flow.rkt
@@ -5,7 +5,7 @@
          (all-from-out "flow/extended/expander.rkt")
          (all-from-out "flow/extended/forms.rkt"))
 
-(require syntax-spec
+(require syntax-spec-v1
          (for-syntax racket/base
                      syntax/parse
                      (only-in "private/util.rkt"
