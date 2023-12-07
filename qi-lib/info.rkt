4a2b8f69986224d695db0866e6c1b2f654b14db1
Siddhartha <sid@countvajhula.com>
2023-12-07 02:50:46 -0700

    declare macro-debugger dependency

--- qi-lib/info.rkt
+++ qi-lib/info.rkt
@@ -4,7 +4,8 @@
 (define collection "qi")
 (define deps '("base"
                ("fancy-app" #:version "1.1")
-               "syntax-spec-v1"))
+               "syntax-spec-v1"
+               "macro-debugger"))
 (define build-deps '())
 (define clean '("compiled" "private/compiled"))
 (define pkg-authors '(countvajhula))
