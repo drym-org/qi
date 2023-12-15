5f1774d721c95016961cbd1b3687e69c758d27c1
Siddhartha <sid@countvajhula.com>
2023-12-15 13:57:37 -0700

    reorder some tests

--- qi-test/tests/flow.rkt
+++ qi-test/tests/flow.rkt
@@ -354,10 +354,19 @@
     (check-equal? ((☯ (~> (as v) (+ v))) 3)
                   3
                   "binds a single value")
+    (check-equal? ((☯ (~> (as v w) (+ v w))) 3 4)
+                  7
+                  "binds multiple values")
+    (check-false ((☯ (~> (as v) live?)) 3)
+                 "binding does not propagate the value")
     (check-equal? ((☯ (~> (-< (as v)
                               _) (+ 3 _ v))) 3)
                   9
                   "reference in a fine template")
+    (check-equal? ((☯ (~> (-< (as v)
+                              _) (+ 3 __ v))) 3)
+                  9
+                  "reference in a blanket template")
     (check-equal? ((☯ (~> (-< (as v)
                               _) (+ 3 v))) 3)
                   9
@@ -366,15 +375,6 @@
                                _) (+ 3 v))) 3)
                   9
                   "reference in a right-chiral partial application")
-    (check-equal? ((☯ (~> (-< (as v)
-                              _) (+ 3 __ v))) 3)
-                  9
-                  "reference in a blanket template")
-    (check-false ((☯ (~> (as v) live?)) 3)
-                 "binding does not propagate the value")
-    (check-equal? ((☯ (~> (as v w) (+ v w))) 3 4)
-                  7
-                  "binds multiple values")
     (check-equal? ((☯ (~> (-< (~> list (as vs))
                               +)
                           (~a "The sum of " vs " is " _)))
