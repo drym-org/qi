bf581a4d73a4a8ff5e9443d671511f345aa49539
Siddhartha <sid@countvajhula.com>
2023-12-16 12:15:24 -0700

    expand de-expander to full core language (except feedback)

--- qi-lib/flow/extended/util.rkt
+++ qi-lib/flow/extended/util.rkt
@@ -15,7 +15,27 @@
                       thread
                       amp
                       tee
-                      relay)
+                      relay
+                      gen
+                      pass
+                      sep
+                      and
+                      or
+                      not
+                      all
+                      any
+                      fanout
+                      group
+                      if
+                      sieve
+                      partition
+                      try
+                      >>
+                      <<
+                      feedback
+                      loop
+                      loop2
+                      clos)
     [(thread
       expr ...)
      #`(~> #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
@@ -23,14 +43,79 @@
       (expr ...))
      (map prettify-flow-syntax (syntax->list #'(expr ...)))]
     [(#%host-expression expr) #'expr]
-    [((~datum amp)
+    [(amp
       expr ...)
      #`(>< #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
-    [((~datum tee)
+    [(tee
       expr ...)
      #`(-< #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
-    [((~datum relay)
+    [(relay
       expr ...)
      #`(== #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
+    [(gen
+      expr ...)
+     #`(gen #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
+    [(pass
+      expr ...)
+     #`(pass #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
+    [(sep
+      expr ...)
+     #`(sep #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
+    [(and
+      expr ...)
+     #`(and #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
+    [(or
+      expr ...)
+     #`(or #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
+    [(not
+      expr ...)
+     #`(not #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
+    [(all
+      expr ...)
+     #`(all #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
+    [(any
+      expr ...)
+     #`(any #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
+    [(fanout
+      expr ...)
+     #`(fanout #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
+    [(group
+      expr ...)
+     #`(group #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
+    [(if
+      expr ...)
+     #`(if #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
+    [(sieve
+      expr ...)
+     #`(sieve #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
+    [(partition
+      [e1 e2] ...)
+     #:with e1-prettified (map prettify-flow-syntax (attribute e1))
+     #:with e2-prettified (map prettify-flow-syntax (attribute e2))
+     #`(partition [e1-prettified e2-prettified])]
+    [(try expr
+      [e1 e2] ...)
+     #:with expr-prettified (prettify-flow-syntax #'expr)
+     #:with e1-prettified (map prettify-flow-syntax (attribute e1))
+     #:with e2-prettified (map prettify-flow-syntax (attribute e2))
+     #`(try expr-prettified [e1-prettified e2-prettified])]
+    [(>>
+      expr ...)
+     #`(>> #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
+    [(<<
+      expr ...)
+     #`(<< #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
+    [(feedback
+      expr ...)
+     #`(feedback #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
+    [(loop
+      expr ...)
+     #`(loop #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
+    [(loop2
+      expr ...)
+     #`(loop2 #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
+    [(clos
+      expr ...)
+     #`(clos #,@(map prettify-flow-syntax (syntax->list #'(expr ...))))]
     [(esc expr) (prettify-flow-syntax #'expr)]
     [expr #'expr]))
