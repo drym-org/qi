ef3ac93068bf0e23e422738cd4f4f4a31acbc858
Siddhartha <sid@countvajhula.com>
2023-12-28 19:08:15 -0700

    Fix "full cycle" test so it runs in phase 1

--- qi-test/tests/compiler/rules/full-cycle.rkt
+++ qi-test/tests/compiler/rules/full-cycle.rkt
@@ -2,27 +2,34 @@
 
 (provide tests)
 
-(require (for-template qi/flow/core/compiler)
+(require (for-syntax racket/base)
          ;; necessary to recognize and expand core forms correctly
          qi/flow/extended/expander
          ;; necessary to correctly expand the right-threading form
          qi/flow/extended/forms
-         (for-syntax racket/base)
          rackunit
          rackunit/text-ui
-         qi/flow/core/private/form-property
-         (only-in "deforest.rkt" deforested?)
-         "../private/expand-util.rkt"
-         syntax/parse/define)
+         syntax/macro-testing
+         (submod qi/flow/extended/expander invoke))
 
-;; A macro that accepts surface syntax, expands it, and then applies the
-;; indicated optimization passes.
-(define-syntax-parser test-compile~>
-  [(_ stx)
-   #'(phase0-expand-flow stx)]
-  [(_ stx pass ... passN)
-   #'(passN
-      (test-compile~> stx pass ...))])
+(begin-for-syntax
+  (require racket/base
+           syntax/parse/define
+           racket/string
+           (for-template qi/flow/core/compiler)
+           (for-syntax racket/base))
+
+  (define (deforested? exp)
+    (string-contains? (format "~a" exp) "cstream"))
+
+  ;; A macro that accepts surface syntax, expands it, and then applies the
+  ;; indicated optimization passes.
+  (define-syntax-parser test-compile~>
+    [(_ stx)
+     #'(expand-flow stx)]
+    [(_ stx pass ... passN)
+     #'(passN
+        (test-compile~> stx pass ...))]))
 
 
 (define tests
@@ -33,10 +40,11 @@
    (test-suite
     "multiple passes"
     (test-true "normalize → deforest"
-               (deforested?
-                 (test-compile~> #'(~>> (filter odd?) values (map sqr))
-                                 normalize-pass
-                                 deforest-pass))))))
+               (phase1-eval
+                (deforested?
+                  (test-compile~> #'(~>> (filter odd?) values (map sqr))
+                                  normalize-pass
+                                  deforest-pass)))))))
 
 (module+ main
   (void
