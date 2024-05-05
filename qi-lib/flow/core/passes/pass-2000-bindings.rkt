e23c813ab0889cb9a635d12872ccd86c434f056f
Dominik Pantůček <dominik.pantucek@trustica.cz>
2024-05-05 15:11:01 +0200

    Finish fixing all the things broken by the rebase.

--- qi-lib/flow/core/passes/pass-2000-bindings.rkt
+++ qi-lib/flow/core/passes/pass-2000-bindings.rkt
@@ -50,8 +50,11 @@
     (let ([ids null])
       (find-and-map/qi (syntax-parser
                          [((~datum as) x ...)
-                          (set! ids
-                                (append (attribute x) ids))]
+                          (begin
+                            (set! ids
+                                  (append (attribute x) ids))
+                            ;; we don't need to traverse further
+                            #f)]
                          [_ this-syntax])
                        stx)
       ids))
