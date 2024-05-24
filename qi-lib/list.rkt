3b53feccad4a41446cc34cc31597afda92969229
Dominik Pantůček <dominik.pantucek@trustica.cz>
2024-05-24 18:59:29 +0200

    Rename compiler passes subdirectory to compiler and strip the passes file name pass- prefix.

--- qi-lib/list.rkt
+++ qi-lib/list.rkt
@@ -3,6 +3,6 @@
 ;; Upon instantiation of the module it define-and-register-pass for
 ;; deforestation
 (require racket/list
-         "flow/core/passes/pass-0100-deforest.rkt")
+         "flow/core/compiler/0100-deforest.rkt")
 
 (provide (all-from-out racket/list))
