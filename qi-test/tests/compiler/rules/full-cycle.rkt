ed369272df6ed08d4e56195b721f126137095dfa
Dominik Pantůček <dominik.pantucek@trustica.cz>
2024-05-24 18:59:29 +0200

    Improve directory and module naming
    - rename compiler "passes" subdirectory to "compiler"
    - strip the passes modules file name pass- prefix

--- qi-test/tests/compiler/rules/full-cycle.rkt
+++ qi-test/tests/compiler/rules/full-cycle.rkt
@@ -10,7 +10,7 @@
          rackunit
          rackunit/text-ui
          syntax/macro-testing
-         qi/flow/core/passes/pass-0100-deforest
+         qi/flow/core/compiler/0100-deforest
          "private/deforest-util.rkt"
          (submod qi/flow/extended/expander invoke))
 
