5038ae5a48218b478130e077490fe5462a5998e5
Dominik Pantůček <dominik.pantucek@trustica.cz>
2024-06-14 10:57:34 +0200

    Split bindings module and add appropriate scribblings.
    - scribblings for qi/list module
    - scribble the new literals for matching in deforestation pass
    - ensure for-label bindings in the generated documentation
    - new bindings.rkt module

--- qi-lib/list.rkt
+++ qi-lib/list.rkt
@@ -3,6 +3,6 @@
 ;; Upon instantiation of the module it define-and-register-pass for
 ;; deforestation
 (require "flow/core/compiler/0100-deforest.rkt"
-         "flow/core/compiler/deforest/binding.rkt")
+         "flow/core/compiler/deforest/bindings.rkt")
 
-(provide (all-from-out "flow/core/compiler/deforest/binding.rkt"))
+(provide (all-from-out "flow/core/compiler/deforest/bindings.rkt"))
